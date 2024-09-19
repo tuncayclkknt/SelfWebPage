40b1ea794f5696281a2d443f7ff72a675e1ff206
tuncayclkknt <tuncayclkknt@gmail.com>
2024-09-19 18:29:24 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        [Key]
+        public int TestimonialID { get; set; }
+        public string ClientName { get; set; }
+        public string Companiy { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
